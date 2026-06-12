d2641ff07cf8506cae2ce485e3c09b396c65f2f7
SanjaiM89 <sanjaim@myyahoo.com>
2026-06-12 15:58:55 +0530

    TASK 1 in django completed

--- notes.py
+++ notes.py
@@ -33,3 +33,42 @@
 # to the request
 # 2) django.contrib.auth.security.SecurityMiddleware - It enforces HTTPS(TLS/SSL Encryption) and adds x-content-type-options and x-xss-protection
 # It protects against most common attacks, it adds http headers to the responses from views.
+# 
+# 3) WSGI vs ASGI
+# 
+# WSGI (WebServer Gateway Interface) :-
+# -> It handles one request at a time per worker
+# -> It cannot handle fullduplex (websocket) or long lived connections
+# -> It is an synchronous connection i.e handling one request at a time
+# -> It takes more time than ASGI execution and uses more resources
+# -> By default Django uses WSGI
+# 
+# ASGI (Asynchronous GateWay Interface):-
+# -> It handles more than one request at a time
+# -> It can handle fullduplex or long lived connections
+# -> It is an asynchronous type connection and also supports sunchronous code
+# -> It uses less resources and produces response time by shorter period
+# 
+# 4) Explain the MVC pattern, then map it to Django's MVT (Model-View-Template): what does each letter correspond to in Django?
+# 
+# MVC :-
+# 
+# M - Model, V - View, C - Controller
+# 
+# * M(Model) -> It manages the application data, It stores and retrieves the data from the database and it applies logic and rules for the 
+#                   requirments of the function
+# * V(View) -> It is responsible for displaying the data to the user, It uses render and serializes the data
+# * C(Controller) -> It handles the user request. It sends the request to the model, model gets the requested data/value and then sends to the 
+#                    data/value to the controller, controller then places the data to the views where views render to display the data
+# 
+# MVT :-
+# 
+# M - Model, V - View, T - Template
+# 
+# Django commonly uses MVT pattern for web appliccations
+# 
+# * M(Model) -> It manages the application data and data strcutures. It applies logic defined and the rules, it stores and retrieves
+#               the data form databases.
+# * V(view) -> It handles request and response logic, it is responsible for getting the http request and get the values/data from
+#               model and render, serialize the data to display to user.
+# * T(Template) -> It is the presentation layer, the html page. It uses Django's Template Language(DTL) for dynamic contents
