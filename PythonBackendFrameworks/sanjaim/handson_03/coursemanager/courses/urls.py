e8bbc5ac2c253e0eccd1450438d324da5ef669dc
SanjaiM89 <sanjai@myyahoo.com>
2026-06-14 15:34:39 +0530

    Task 2 step 31 completed

--- PythonBackendFrameworks/sanjaim/handson_03/coursemanager/courses/urls.py
+++ PythonBackendFrameworks/sanjaim/handson_03/coursemanager/courses/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from .views import CourseListView,CourseDetailView
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
 
+# from .views import CourseListView,CourseDetailView
+from .views import CourseViewset
+
+route = DefaultRouter()
+route.register(r"", CourseViewset)
 urlpatterns = [
-    path('',CourseListView.as_view(),name='course_list'),
-    path('<int:pk>/',CourseDetailView.as_view(),name='course_detail')
+    # path('',CourseListView.as_view(),name='course_list'),
+    # path('<int:pk>/',CourseDetailView.as_view(),name='course_detail')
+    #
+    path("", include(route.urls))
 ]
