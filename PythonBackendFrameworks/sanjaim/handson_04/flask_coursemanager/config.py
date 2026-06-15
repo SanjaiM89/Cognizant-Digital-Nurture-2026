e8c675906ee29317cec857076fe4647c88589af1
SanjaiM89 <sanjai@myyahoo.com>
2026-06-15 07:00:36 +0530

    Flask, Task 1 step 38 completed

---
+++ PythonBackendFrameworks/sanjaim/handson_04/flask_coursemanager/config.py
@@ -0,0 +1,5 @@
+class config:
+    SQL_ALCHEMY_DATABASE_URI = "postgresql://postgres:Cvpy24kp@2005@db.vvizsxnxxlrwvepulmkk.supabase.co:5432/postgres"
+    SECRET_KEY = '5531b788bd4802d6d6b7c34bdc6304b9f8c5a96d392f35d00bf922320c9fd7c2'
+    DEBUG = True
+
